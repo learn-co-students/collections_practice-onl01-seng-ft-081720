ac67a3c3ccab84c9bd1ee14b4bfb7cddc287411c
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 03:31:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array = array.sort
+  array
+end 
 
+def sort_array_desc(array)
+  array = array.sort.reverse
+  array
+end 
+
+def sort_array_char_count(array)
+  array = array.sort_by(&:length)
+  array
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array = array.reverse
+  array
+end 
+
+def kesha_maker(array)
+  array.each do |char|
+    char[2] = "$"
+  end 
+end 
+
+def find_a(array)
+  array.select do |a|
+    a.start_with? ("a")
+  end 
+end 
+
+def sum_array(array)
+    array.inject do |sum,num| 
+        sum + num
+    end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word,index|
+        if index != 1 
+          "#{word}s"
+        else
+          word = word
+        end
+  end
+end
