9e587c263c94d2609737e37565038c34e2795a30
Richard Kurwitz <kurwitz3@aol.com>
2020-08-20 18:48:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+array.sort 
+end
+def sort_array_desc(array)
+  array.sort!{|x,y| y <=> x}
+end
+ def sort_array_char_count(array)
+   array.sort {|a,b| a.length <=> b.length}
+   end
+ def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   array
+ 
+ end
+ def reverse_array(array)
+   array.reverse
+ end
+def kesha_maker(array)
+   money = []
+  array.each do |x|
+  x[2] = "$"
+  money << x 
+  end 
+  money
+end 
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+def sum_array(array)
+   total = array.inject{|x,z| x + z}
+total 
+  end
+  def add_s(array)
+    new = []
+    array.each_with_index  do |element,index|
+      if index != 1 
+      element << ("s")
+    end
+    end
 
+  end
+    
