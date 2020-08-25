68ec85f2b9481a79d5dba42173df60804b30bc36
John Glennan <glennan20@gmail.com>
2020-08-25 14:37:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.size <=> y.size}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  array.collect {|letter| letter[2] = "$"}
+  array
+end
+
+def find_a (array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array (array)
+  array.inject {|sum, num| sum + num}
+end  
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
