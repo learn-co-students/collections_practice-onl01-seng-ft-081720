7d29375a81a18584bc16a9651c71549c8dc62e5b
SophiaGrace16 <sophia.g.white@outlook.com>
2020-08-25 15:15:01 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc (array)
+    array.sort
+end
 
+def sort_array_desc (array)
+    array.sort.reverse!
+end
+
+def sort_array_char_count (array)
+    array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements (array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array (array)
+    array.reverse!
+end
+
+def kesha_maker (array)
+    array.each {|index| index[2] = "$"}
+end
+
+def find_a (array)
+    array.select {|index| index.start_with? "a"}
+end
+
+def sum_array (array)
+    array.inject {|sum, number| sum + number}
+end
+
+def add_s (array)
+    array.collect{|arr| arr==array[1] ? arr:arr <<"s"}
+end
